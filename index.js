8194afe7aa576e377c07735d80709548ae38adfe
pizzaandpast4 <dmitrij.fiodorov@gmail.com>
2024-08-30 09:57:38 +0300

    isadult

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-let accounts = {}; 
+let accounts = {};
+
+const isAdult = (dob) => {
+    const birthDate = new Date(dob);
+    const age = new Date().getFullYear() - birthDate.getFullYear();
+    return age >= 18;
