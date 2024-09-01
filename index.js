266c6c36473285ea701e9f2809b512a4d0fdecb6
pizzaandpast4 <dmitrij.fiodorov@gmail.com>
2024-09-01 23:59:07 +0300

    server

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const app = express();
 app.use(express.json());
 
 let accounts = {};
+const port = 3333;
 
 const isAdult = (dob) => {
     const birthDate = new Date(dob);
     const age = new Date().getFullYear() - birthDate.getFullYear();
     return age >= 18;
-};    
+};
+
+app.listen(port, () => {
+    console.log(`App running on: http://localhost:${port}`);
+});
